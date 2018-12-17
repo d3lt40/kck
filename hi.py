08a71c7522127929b53af5f5fd7d8fb142b0b5f5
d3lt40 <45161932+d3lt40@users.noreply.github.com>
2018-12-17 09:19:50 +0100

    ZADANKO

---
+++ hi.py
@@ -0,0 +1,13 @@
+
+import aseegg as ag
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dane = pd.read_csv("dane.csv", delimiter=';', engine='python')
+mojeDane = dane['Sub02']
+przefiltrowany= ag.pasmowozaporowy(mojeDane, 256, 49, 51)
+przefiltrowany=ag.gornoprzepustowy(mojeDane, 256, 3)
+ag.spektrogram(przefiltrowany, 256)
+## 8hz 8-20 seksunDA
+plt.plot(mojeDane)
+plt.show()
